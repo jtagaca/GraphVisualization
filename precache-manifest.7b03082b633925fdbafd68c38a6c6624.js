27cc2c3d207e3692a538de915a781c45a261e113
jtagaca <jtagaca@csub.edu>
2021-06-06 19:18:01 -0700

    Updates

---
+++ precache-manifest.7b03082b633925fdbafd68c38a6c6624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32014f68917e3f2be6b2443205d9e4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52af3a79b43c30ac5a96",
+    "url": "/static/css/main.9528f765.chunk.css"
+  },
+  {
+    "revision": "91e648ac11490d4f0922",
+    "url": "/static/js/2.0e7a2599.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.0e7a2599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52af3a79b43c30ac5a96",
+    "url": "/static/js/main.09b91177.chunk.js"
+  },
+  {
+    "revision": "df64ef7eadc763db950e",
+    "url": "/static/js/runtime-main.22660e7b.js"
+  }
+]);
